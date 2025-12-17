d10533fa52787803efdec8fc000a31c9944e96e5
Tejas Kuranjekar <tejaskuranjekar10@gmail.com>
2025-12-17 14:07:18 +0530

    fix script.js bugs

--- public/js/script.js
+++ public/js/script.js
@@ -1,50 +1,50 @@
 alert("script.js loaded");
 
-const socket = io("https://YOUR-BACKEND-URL.onrender.com");
+const socket = io("https://realtime-location-backend.onrender.com");
 
+const map = L.map("map").setView([0, 0], 16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "RealTimeLocation-Tejas"
+}).addTo(map);
+
+const markers = {};
 
 if (navigator.geolocation) {
-    navigator.geolocation.watchPosition(
-        (position) => {
-            const { latitude, longitude } = position.coords;
-            socket.emit("send-location", { latitude, longitude });
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      socket.emit("send-location", { latitude, longitude });
     },
-    (error)=>{
-        console.error(error);
+    (error) => {
+      console.error(error);
+      alert("Location permission denied");
     },
     {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
     }
-    );
-}{
-
+  );
+} else {
+  alert("Geolocation not supported");
 }
 
-const map = L.map("map").setView([0,0], 16);
+socket.on("receive-location", (data) => {
+  const { id, latitude, longitude } = data;
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-    attribution: "RealTimeLocation-Tejas"
-}).addTo(map)
+  map.setView([latitude, longitude]);
 
-const markers ={};
-
-socket.on("receive-location", (data)=>{
-    const {id, latitude, longitude} = data;
-    map.setView([latitude, longitude]);
-    if (markers[id]) {
-        markers[id].setLatLng([latitude, longitude]);
-       }
-       else{
-       markers[id] = L.marker([latitude, longitude]).addTo(map);
-    }
-
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
 });
 
-socket.on("user-disconnect",(id) => {
-    if(marker[id]){
-        map.removeLayer(markers[id]);
-        delete marker[id];
-    }
-})
+socket.on("user-disconnected", (id) => {
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
