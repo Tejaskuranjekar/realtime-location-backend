03890012b6656d9da8b1956e32b770d395827762
Tejas Kuranjekar <tejaskuranjekar10@gmail.com>
2025-12-17 13:14:34 +0530

    backend ready for render

---
+++ public/js/script.js
@@ -0,0 +1,48 @@
+const socket = io("https://YOUR-BACKEND-URL.onrender.com");
+
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+            socket.emit("send-location", { latitude, longitude });
+    },
+    (error)=>{
+        console.error(error);
+    },
+    {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    }
+    );
+}{
+
+}
+
+const map = L.map("map").setView([0,0], 16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+    attribution: "RealTimeLocation-Tejas"
+}).addTo(map)
+
+const markers ={};
+
+socket.on("receive-location", (data)=>{
+    const {id, latitude, longitude} = data;
+    map.setView([latitude, longitude]);
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+       }
+       else{
+       markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+
+});
+
+socket.on("user-disconnect",(id) => {
+    if(marker[id]){
+        map.removeLayer(markers[id]);
+        delete marker[id];
+    }
+})
